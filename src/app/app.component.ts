c86c961aee66b1917b4e9e7a0d8d87173fa9809a
lichenbuliren <617106268@qq.com>
2016-10-10 12:27:41 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { CarouselComponent } from './carousel/carousel.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Renderer, AfterViewInit, style, keyframes, animate} from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor(private el: ElementRef, private renderer: Renderer) {
+    console.log('in constructor: ' + (+new Date()));
+    console.log(this.el.nativeElement);
 
-  ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    console.log('in ngAfterViewInit: ' + (+new Date()));
+    console.log(this.el.nativeElement);
+    this.renderer.setElementStyle(this.el.nativeElement, 'color', '#333bb7');
+    this.renderer.animate(this.el.nativeElement, {
+      styles: [{
+        'opacity': 1,
+        'transform': 'translateX(0)'
+      }]
+    }, [], 300, 100, 'ease-in-out');
   }
 
 }
